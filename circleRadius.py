89344a529a67204899ca35ce0603ce28393248d2
Tobechukwu816094 <ugbaajat21@gmail.com>
2020-06-30 11:53:37 +0100

    first commit

---
+++ circleRadius.py
@@ -0,0 +1,10 @@
+
+import math
+radius = float (input("input the radius of the circle:"))
+area  = math . pi*radius**2
+print ("the area of the circle with radius" + str(radius)+" is:"+str(area))
+
+
+
+
+
